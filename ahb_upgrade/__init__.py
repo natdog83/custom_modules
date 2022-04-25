80a1754d897750fb1e9a76c5c885d8f0f80a776d
Nathan McCusker <nathan@nathanmccusker.com>
2022-04-24 21:42:56 -0700

    Adding ahb_upgrade module

---
+++ ahb_upgrade/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# from . import controllers
+# from . import models
+# from . import demo
+# from . import wizard
+# from . import report
+# from . import populate
+#
+# from odoo import api, SUPERUSER_ID
