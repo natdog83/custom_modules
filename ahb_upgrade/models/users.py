e4779bfa778c1ca705c3eb448102244ce736e74c
Nathan McCusker <nathan@nathanmccusker.com>
2022-04-26 22:37:25 -0700

    Adding old id field to partner view

--- ahb_upgrade/models/users.py
+++ ahb_upgrade/models/users.py
@@ -1,7 +1,7 @@
 from odoo import api, fields, models
 
 class ResUser(models.Model):
-    _name = 'res.user'
-    _inherit = 'res.user'
+    _name = 'res_user'
+    _inherit = 'res_user'
 
     old_id = fields.Char(string='Old ID', readonly=True)
